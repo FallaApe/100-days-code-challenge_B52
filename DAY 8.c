56d8ee9204f290374fd94cf1a026a23f90089930
Shivam Sharma <godshivam12007@gmail.com>
2025-10-15 23:33:08 +0530

    question 15 and 16

--- DAY 8.c
+++ DAY 8.c
@@ -1,4 +1,28 @@
-//question 15
+//Q15: Write a program to input a character and check whether it is an uppercase alphabet, lowercase alphabet, digit, or special character.
+
+/*
+Sample Test Cases:
+Input 1:
+A
+Output 1:
+Uppercase alphabet
+
+Input 2:
+a
+Output 2:
+Lowercase alphabet
+
+Input 3:
+3
+Output 3:
+Digit
+
+Input 4:
+#
+Output 4:
+Special character
+
+*/
 #include <stdio.h>
 void main() {
     char a;
@@ -13,7 +37,21 @@ void main() {
     }  
 } 
 
-//qiestion 16
+//Q16: Write a program to input three numbers and find the largest among them using if–else.
+
+/*
+Sample Test Cases:
+Input 1:
+3 7 5
+Output 1:
+Largest is 7
+
+Input 2:
+-1 -5 0
+Output 2:
+Largest is 0
+
+*/
 #include <stdio.h>
 void main() {
     int a,b,c;
