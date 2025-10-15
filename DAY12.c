f449f6c26cc6e3954a017399455b0f45f697768f
Shivam Sharma <godshivam12007@gmail.com>
2025-10-15 23:35:29 +0530

    question 23 and 24

--- DAY12.c
+++ DAY12.c
@@ -1,4 +1,32 @@
-//question 23
+//Q23: Write a program to calculate library fine based on late days as follows: 
+First 5 days late: ₹2/day 
+Next 5 days late: ₹4/day 
+Next 20 days days late: ₹6/day 
+More than 30 days: Membership Cancelled.
+
+/*
+Sample Test Cases:
+Input 1:
+4
+Output 1:
+Fine ₹8
+
+Input 2:
+8
+Output 2:
+Fine ₹22
+
+Input 3:
+15
+Output 3:
+Fine ₹60
+
+Input 4:
+31
+Output 4:
+Membership Cancelled
+
+*/
 #include <stdio.h>
 void main() {
     int days, fine = 0;
@@ -18,7 +46,30 @@ void main() {
         printf("Fine %d", fine);
 }
 
-//question 24
+//Q24: Write a program to calculate electricity bill based on units consumed with these rates: 
+First 100 units at ₹5/unit 
+Next 100 units at ₹7/unit 
+Next 100 units at ₹10/unit 
+Above at ₹12/unit
+
+/*
+Sample Test Cases:
+Input 1:
+50
+Output 1:
+Bill: ₹250
+
+Input 2:
+150
+Output 2:
+Bill: ₹850
+
+Input 3:
+250
+Output 3:
+Bill: ₹1700
+
+*/
 #include <stdio.h>
 void main() {
     int units, bill = 0;
