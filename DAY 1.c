117a9f4296bb404ec307d1af6a303342696596c9
Shivam Sharma <godshivam12007@gmail.com>
2025-10-15 19:22:18 +0530

    question 1 and 2

---
+++ DAY 1.c
@@ -0,0 +1,23 @@
+//question 1
+#include <stdio.h>
+void main() {
+    int a,b;
+    printf("enter two numbers:");
+    scanf("%d%d",&a,&b);
+    int sum = a+b;
+    printf("sum = %d",sum);
+}
+
+//question 2
+#include <stdio.h>
+void main() {
+    int a,b;
+    printf("enter two numbers:");
+    scanf("%d%d",&a,&b);
+    int sum = a+b;
+    int diff = a-b;
+    int prod = a*b;
+    int quot = a/b;
+    printf("sum = %d, diff =%d, product = %d, Quotient = %d ",sum,diff,prod,quot);
+}
+ 
