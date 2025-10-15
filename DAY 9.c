911d89792f857539643cb881fd4926552356aed3
Shivam Sharma <godshivam12007@gmail.com>
2025-10-15 23:33:43 +0530

    question 17 and 18

--- DAY 9.c
+++ DAY 9.c
@@ -1,4 +1,23 @@
-//question 17
+//Q17: Write a program to find the roots of a quadratic equation and categorize them.
+
+/*
+Sample Test Cases:
+Input 1:
+1 -3 2
+Output 1:
+Roots are real and different: 2, 1
+
+Input 2:
+1 -2 1
+Output 2:
+Roots are real and same: 1
+
+Input 3:
+1 2 5
+Output 3:
+Roots are complex
+
+*/
 #include <stdio.h>
 #include <math.h>
 
@@ -17,7 +36,36 @@ void main() {
         printf("Roots are imaginary");
     }
 }
-//question 18
+//Q18: Write a program that accepts a percentage (0-100) and assigns a grade based on the following criteria: 
+90-100: Grade A 
+80-89: Grade B 
+70-79: Grade C 
+60-69: Grade D 
+below 60: Grade F.
+
+/*
+Sample Test Cases:
+Input 1:
+95
+Output 1:
+Grade A
+
+Input 2:
+82
+Output 2:
+Grade B
+
+Input 3:
+68
+Output 3:
+Grade D
+
+Input 4:
+50
+Output 4:
+Grade F
+
+*/
 #include <stdio.h>
 void main() {
     float percent;
