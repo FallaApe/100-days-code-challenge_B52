af898fb16ba5b1158d285eed2eb3f6dbb19a2b39
Shivam Sharma <godshivam12007@gmail.com>
2025-10-15 23:30:36 +0530

    question 7 and 8

--- DAY 4 .c
+++ DAY 4 .c
@@ -1,4 +1,18 @@
-//question 7
+//Q7: Write a program to swap two numbers without using a third variable.
+
+/*
+Sample Test Cases:
+Input 1:
+10 20
+Output 1:
+After swap: 20 10
+
+Input 2:
+7 14
+Output 2:
+After swap: 14 7
+
+*/
 #include <stdio.h>
 void main() {
     int a,b;
@@ -7,7 +21,21 @@ void main() {
     printf("after swap= %d %d",b,a);
 }
 
-//question 8
+//Q8: Write a program to find and display the sum of the first n natural numbers.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+Output 1:
+Sum=15
+
+Input 2:
+10
+Output 2:
+Sum=55
+
+*/
 #include <stdio.h>
 void main() {
     int a,sum=0;
