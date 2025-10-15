926391a1f53a57063221e6aae226e9e97671d3bc
Shivam Sharma <godshivam12007@gmail.com>
2025-10-15 19:24:54 +0530

    question 5 and 6

---
+++ DAY 3.c
@@ -0,0 +1,19 @@
+//question 5
+#include <stdio.h>
+void main() {
+    float c;
+    printf("enter temp in celsius: ");
+    scanf("%f",&c);
+    float f = (c*9/5)+32;
+    printf("temp in fahrenheit is %f",f);
+} 
+
+//question 6
+#include <stdio.h>
+void main() {
+    int a,b;
+    printf("enter two numbers:");
+    scanf("%d%d",&a,&b);
+    printf("after swap: %d, %d",b,a);
+}
+ 
