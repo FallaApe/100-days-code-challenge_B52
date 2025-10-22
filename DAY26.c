ae336810f37f74c89eaa2ecba1f2bb413166ae65
Shivam Sharma <godshivam12007@gmail.com>
2025-10-22 20:14:09 +0530

    Create DAY26.c

---
+++ DAY26.c
@@ -0,0 +1,36 @@
+//Q51: Write a program to print the following pattern:
+    5
+   45
+  345
+ 2345
+12345
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+    5
+   45
+  345
+ 2345
+12345
+
+*/
+#include <stdio.h>
+void main() {
+    int a;
+    printf("Enter number: ");
+    scanf("%d", &a);
+
+    for (int i = a; i >= 1; i--) {
+        for (int s = 1; s < i; s++) {
+            printf(" ");
+        }
+        for (int j = i; j <= a; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+}
+//
