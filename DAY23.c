b10ac9ddd2271bd01a5825cbb241932e5b463399
Shivam Sharma <godshivam12007@gmail.com>
2025-10-22 19:49:37 +0530

    question 45 and 46

--- DAY23.c
+++ DAY23.c
@@ -1,3 +1,35 @@
+//Q45: Write a program to find the sum of the series: 2/3 + 4/7 + 6/11 + 8/15 + ... up to n terms.
+
+/*
+Sample Test Cases:
+Input 1:
+3
+Output 1:
+Approximate sum: 1.56
+
+Input 2:
+5
+Output 2:
+Approximate sum: 2.22
+
+*/
+#include <stdio.h>
+void main() {
+    int a;
+    printf("Enter number: ");
+    scanf("%d", &a);
+    
+    float n = 2.0, d = 3.0;
+    float sum = 0.0;
+
+    for (int i = 1; i <= a; i++) {
+        sum += n / d;   
+        n += 2;
+        d += 4;
+    }
+
+    printf("Approximate sum: %.2f\n", sum);
+}
 
 //Q46: Write a program to print the following pattern:
 *****
