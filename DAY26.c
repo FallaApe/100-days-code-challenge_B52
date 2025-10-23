7f788e45d2f2f73e99136695cd6abc3409030eb5
Shivam Sharma <godshivam12007@gmail.com>
2025-10-23 11:38:07 +0530

    question 51 and 52

--- DAY26.c
+++ DAY26.c
@@ -33,4 +33,49 @@ void main() {
         printf("\n");
     }
 }
-//
+//Q52: Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with stars spaced irregularly as shown.
+
+*/
+#include <stdio.h>
+
+void main() {
+    for (int i = 1; i <= 9; i++) {
+        if (i == 1 || i == 9) {
+            printf("*\n\n");
+        }
+        else if (i >= 2 && i <= 4) {
+            printf("*\n");
+            if (i == 4) printf("\n");
+        }
+        else if (i >= 5 && i <= 8) {
+            printf("*\n");
+        }
+    }
+}
