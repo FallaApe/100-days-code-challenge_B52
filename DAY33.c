d9260b1115b2e926a0c064539a4b24b4deef9512
Shivam Sharma <godshivam12007@gmail.com>
2025-10-28 11:02:55 +0530

    question 65 and 66

--- DAY33.c
+++ DAY33.c
@@ -39,4 +39,46 @@ void main() {
         }
     }
 }
-//
+//Q66: Insert an element in a sorted array at the appropriate position.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+1 2 4 5 6
+3
+Output 1:
+1 2 3 4 5 6
+
+*/
+#include <stdio.h>
+
+void main() {
+    int n;
+    printf("Enter array size: ");
+    scanf("%d", &n);
+
+    int arr[n + 1]; 
+    printf("Enter sorted elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int m;
+    printf("Enter element to insert: ");
+    scanf("%d", &m);
+
+    int i = n - 1;
+    while (i >= 0 && arr[i] > m) {
+        arr[i + 1] = arr[i];
+        i--;
+    }
+
+    arr[i + 1] = m; 
+    n++;
+
+    printf("Updated array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+}
