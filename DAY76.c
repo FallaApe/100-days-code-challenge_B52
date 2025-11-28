91f88b2d875f65c61fcfc5883e5cdc827ce568c4
Shivam Sharma <godshivam12007@gmail.com>
2025-11-28 11:25:11 +0530

    question 126

--- DAY76.c
+++ DAY76.c
@@ -1,6 +1,6 @@
 //Q126: Ask the user for a filename. Check if it exists by trying to open it in read mode. If the file pointer is NULL, print an error message; otherwise, read and display its content.
 
-/*
+/* 
 Sample Test Cases:
 Input 1:
 Filename: sample.txt (File Exists: Yes)
